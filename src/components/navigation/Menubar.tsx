4973ced9f1b79f5a02998f0e372806d118ddeaab
Evelyn Vania <>
2025-08-07 18:19:57 +0800

    updated navbar transition, fixed contact hover
    added transition and color on hover, removed bg-accent on hover, fixed Contact button hover behavior, pinned navbar on top

--- src/components/navigation/Menubar.tsx
+++ src/components/navigation/Menubar.tsx
@@ -18,7 +18,7 @@ export const Menubar = () => {
           {MENU_ITEMS.map((item, index) => (
             <NavigationMenuLink
               key={index}
-              className={`${navigationMenuTriggerStyle()} cursor-pointer rounded-full`}
+              className={`${navigationMenuTriggerStyle()} cursor-pointer rounded-full transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:text-magenta`}
             >
               <div>{item.title}</div>
             </NavigationMenuLink>
@@ -26,7 +26,7 @@ export const Menubar = () => {
         </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuLink
-            className={`${navigationMenuTriggerStyle()} contact-button cursor-pointer rounded-full px-4 py-2 transition-colors duration-200`}
+            className={`${navigationMenuTriggerStyle()} contact-button cursor-pointer rounded-full px-4 py-2 transition duration-300 `}
           >
             <div>Contact</div>
           </NavigationMenuLink>
