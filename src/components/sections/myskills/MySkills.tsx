3f4da0bf4882838e7a1e6d9c67c3ac48250ab3e0
Ahti Ruusuvuori <ahti.ruusuvuori@gmail.com>
2025-09-08 18:43:54 +0300

    Added images nd slide animations to uiux

--- src/components/sections/myskills/MySkills.tsx
+++ src/components/sections/myskills/MySkills.tsx
@@ -3,6 +3,9 @@ import { animate, createScope } from "animejs";
 import { Title } from "@/components/common/title/Title";
 import { TITLE, UIUX } from "./constants";
 import chibi from "@/assets/images/uiux-kabe-chibi.png";
+import icons from "@/assets/images/uiux-icons.png";
+import devices from "@/assets/images/uiux-devices.png";
+import gradient from "@/assets/images/uiux-gradient.png";
 import "./myskills.css";
 
 export const MySkills: React.FC = () => {
@@ -14,9 +17,38 @@ export const MySkills: React.FC = () => {
       if (!self) return;
       self.add("slideIn", () => {
         animate(".uiux-chibi", {
-          translateX: [-500, 0],
-          ease: "out(4)",
-          duration: 1500,
+          translateX: "-50%",
+          ease: "linear",
+        });
+        animate(".uiux-icons", {
+          translateX: "-50%",
+          ease: "linear",
+        });
+        animate(".uiux-devices", {
+          translateX: "-50%",
+          ease: "linear",
+        });
+        animate(".uiux-gradient", {
+          translateX: "-50%",
+          ease: "linear",
+        });
+      });
+      self.add("slideOut", () => {
+        animate(".uiux-chibi", {
+          translateX: "50%",
+          ease: "linear",
+        });
+        animate(".uiux-icons", {
+          translateX: "50%",
+          ease: "linear",
+        });
+        animate(".uiux-devices", {
+          translateX: "50%",
+          ease: "linear",
+        });
+        animate(".uiux-gradient", {
+          translateX: "50%",
+          ease: "linear",
         });
       });
     });
@@ -30,13 +62,20 @@ export const MySkills: React.FC = () => {
     scope.current.methods.slideIn();
   };
 
+  const handleSlideOut = () => {
+    if (!scope.current) return;
+
+    scope.current.methods.slideOut();
+  };
+
   return (
     <div>
       <Title text={TITLE.text} tooltipText={TITLE.tooltipText} />
       <div ref={root}>
         <div
           onMouseOver={handleSlideIn}
-          className="uiux-container h-96 flex justify-between items-center bg-white hover:bg-gradient-to-r hover:from-[#C82E5B] hover:to-[#FFC862] transition-colors duration-500 ease-in-out shadow-lg cursor-pointer"
+          onMouseLeave={handleSlideOut}
+          className="uiux-container h-96 flex justify-between items-center bg-white hover:bg-gradient-to-r hover:from-[#C82E5B] hover:to-[#FFC862] transition-colors duration-500 ease-in-out shadow-lg cursor-pointer overflow-x-hidden relative"
         >
           <div className="flex flex-col items-center gap-4">
             <p className="uiux-text ml-32 text-9xl bg-gradient-to-r from-[#C82E5B] to-[#FFC862] text-transparent bg-clip-text">
@@ -47,7 +86,26 @@ export const MySkills: React.FC = () => {
             </p>
           </div>
           <div>
-            <img src={chibi} alt="UI/UX Icon" className="uiux-chibi" />
+            <img
+              src={chibi}
+              alt="Kabe"
+              className="uiux-chibi translate-x-[50%] absolute bottom-0 right-0 z-4"
+            />
+            <img
+              src={icons}
+              alt="UI/UX Icons"
+              className="uiux-icons translate-x-[50%] absolute bottom-0 right-0 z-2"
+            />
+            <img
+              src={devices}
+              alt="UI/UX Devices"
+              className="uiux-devices translate-x-[50%] absolute bottom-0 right-0 z-1"
+            />
+            <img
+              src={gradient}
+              alt="UI/UX Devices"
+              className="uiux-gradient translate-x-[50%] absolute bottom-0 right-0 z-0"
+            />
           </div>
         </div>
         <div className="h-48 my-4 mx-16 flex justify-center items-center gap-4">
