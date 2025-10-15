1789a2087c34971a61973437bf8e04bf68086d86
Ahti Ruusuvuori <ahti.ruusuvuori@gmail.com>
2025-10-15 18:18:40 +0300

    Added testimonies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Navigation } from "@/components/sections/navigation/Navigation";
 import { Uiux } from "@/components/sections/uiux/Uiux";
-import { Portfolio3d } from "./components/sections/3d/Portfolio3d";
-import { MySkills } from "./components/sections/myskills/MySkills";
+import { Portfolio3d } from "@/components/sections/3d/Portfolio3d";
+import { MySkills } from "@/components/sections/myskills/MySkills";
+import { Testimonies } from "@/components/sections/testimonies/Testimonies";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <MySkills />
       <Uiux />
       <Portfolio3d />
+      <Testimonies />
     </div>
   );
 };
